12bc03283ac41053611f71ba7cc2f5aa050bd510
Anya Tran <panhtran249@gmail.com>
2015-02-26 21:56:03 -0500

    fixed styling

--- src/main.rs
+++ src/main.rs
@@ -10,36 +10,23 @@ fn main() {
     };
 
     let mut file_buff = io::BufferedReader::new(file);
-    let mut graph: Box<HashMap<String, Vec<String>>> = Box::new(load_graph(file_buff)); //////
-    print_graph(&*graph);
-    io::stdio::print("->");
-     for line in io::stdin().lock().lines() {
-        //io::stdio::print("->");
-        println!("enter");
+    let mut graph: Box<HashMap<String, Vec<String>>> = Box::new(load_graph(file_buff)); 
+    io::stdio::print("-> ");
+    for line in io::stdin().lock().lines() {
         let input_line: String = line.unwrap();
         
         let input: Vec<&str> = input_line.as_slice().split(' ').collect();
         
         let path = find_path(&*graph, input[0].to_string(), input[1].trim_matches('\n').to_string());
-        println!("{:?}", path);
-        io::stdio::print("->");
+        for n in path.iter() {
+            io::stdio::print(format!("{} ", n).as_slice());
+        }
+        io::stdio::print("\n");
+        io::stdio::print("-> ");
     }
     
 }
 
-fn print_graph(graph: &HashMap<String, Vec<String>>) {
-    for (node, neighbors) in graph.iter() {
-        let mut neighbor_string: String = String::new();
-        for neighbor in neighbors.iter() {
-            if neighbor_string.len() > 0 {
-                neighbor_string = format!("{}, {}", neighbor_string, neighbor);
-            } else {
-                neighbor_string = format!("{}", neighbor);
-            }
-        }
-        println!("{}->[{}]", node, neighbor_string);
-    }
-}
 
 
 // read the file and load the graph
@@ -53,11 +40,11 @@ fn load_graph<R: Reader> (mut content: io::BufferedReader<R>) -> HashMap<String,
     for line in content.lines() {
         match line {
             Ok(l) => {
-                // TODO: could we iterate over this while building graph?
                 let mut node: Vec<&str> = l.as_slice().split(' ').collect();
-                let node_name: &str = node.iter().nth(0).unwrap().trim_matches('\n');
-                let n = node.as_slice().slice_from(1);
-                let neighbors: Vec<String> = n.iter().map(|&x| x.trim_matches('\n').to_string()).collect();
+                //let node_name: &str = node.iter().nth(0).unwrap().trim_matches('\n');
+                //let n = node.as_slice().slice_from(1);
+                let node_name: &str = node.remove(0);
+                let neighbors: Vec<String> = node.iter().map(|&x| x.trim_matches('\n').to_string()).collect();
                 match graph_result.entry(node_name.to_string()) {
                     Vacant(entry) => { entry.insert(neighbors); },
                     Occupied(entry) => panic!("Duplicate entry: {}", node_name),
@@ -71,22 +58,16 @@ fn load_graph<R: Reader> (mut content: io::BufferedReader<R>) -> HashMap<String,
 
 ///find a path in a given graph
 fn find_path(mut graph: &HashMap<String, Vec<String>>, start: String, end: String) -> Vec<String> {
-    println!("in find_path");
     let mut current: String = start;
     let mut todo: Vec<String> = vec![];
     let mut visited: Vec<String> = vec![];
     loop {
-        
-        println!("current: {}", current);
-        println!("todo: {:?}", todo);
-        println!("visited: {:?}", visited);
         if (current.as_slice() == end.as_slice()) {
             visited.push(current);
             break;
         }
         let neighbors = graph.get(&current).unwrap();
         for n in neighbors.iter() {
-        /////// duplicates
             if (visited.as_slice().contains(n)) {
                 continue;
             }
